c1503afcc5f07d2fe265030a8ecc11e4de729261
gen <dellosa.genesis.dev@gmail.com>
2022-05-11 15:56:24 +0800

    Added Links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,24 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import { ConnectButton, Typography } from 'web3uikit'
+import { ConnectButton } from 'web3uikit'
 
 const Header = () => {
   return (
-    <div style={{
-      width: '100%', height: 65, display:'flex', alignItems:'center', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)'
+    <div 
+    style={{
+      width: '100%', 
+      height: 65, 
+      display:'flex', 
+      alignItems:'center', 
+      boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)'
     }}>
-      <p>NextJs Demo App</p>
+      <p 
+        style={{
+          fontFamily: 'Helvetica', 
+          fontSize: '18px', 
+          fontWeight: 'bold', 
+          marginLeft: 15
+          }}>NextJs Demo App</p>
       <div style={{position: 'absolute', right: 10}}>
-      <ConnectButton/>
+        <ConnectButton/>
       </div>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,45 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import { ConnectButton } from 'web3uikit'
+import { Button } from 'web3uikit'
 import Header from '../components/header'
+import { useMoralis } from 'react-moralis'
+import { useRouter } from 'next/router'
 
 const Home: NextPage = () => {
+  const { user } = useMoralis()
+  const router = useRouter()
   return (
+    <>
     <Header/>
+    <div>
+      <h2 style={{textAlign: 'center'}}>This is a demo app that shows how to construct a protected route if the user has an NFT.</h2>
+      <h3 style={{textAlign: 'center'}}>Home Page is non protected route, while NFT Page is a protected route.</h3>
+      <h4 style={{textAlign: 'center'}}>If user clicks on NFT Page link and does not have an NFT, user will be redirected to back here.</h4>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+      {user !== null ?
+      <>
+      <div style={{padding: 15}}>
+        <Button
+          id="test-button-link"
+          onClick={() => router.push('/')}
+          text="Home Page"
+          theme="link"
+          type="button"
+        />
+      </div>
+      <div style={{padding: 15}}>
+        <Button
+          id="test-button-link"
+          onClick={() => window.location.href= '/nft'}
+          text="NFT Page"
+          theme="link"
+          type="button"
+        />
+      </div>
+      </>
+      : <h1 style={{marginTop: 50}}>Click on Connect Wallet to Start</h1>}
+      </div>
+    </div>
+    </>
   )
 }
 
