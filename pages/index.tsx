afda4fde0e1ba7ef4d267cf577fd3c98a354cc62
gen <dellosa.genesis.dev@gmail.com>
2022-05-11 15:14:11 +0800

    Implemented header in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 import { ConnectButton } from 'web3uikit'
+import Header from '../components/header'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <ConnectButton/>
-    </div>
+    <Header/>
   )
 }
 
